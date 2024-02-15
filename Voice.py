499b2e8733e5f19ba07992b7dc1b36aabdc20728
Honey Patel <honey.p@crestinfosystems.com>
2024-02-15 12:28:25 +0530

    Changed

--- Voice.py
+++ Voice.py
@@ -7,6 +7,16 @@ import pyaudio
 import wave
 import speech_recognition as sr
 
+import pyaudio
+
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
 load_dotenv()
 
 hugging_face_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
@@ -47,7 +57,8 @@ if start_button:
     # Open a stream to record the audio
     stream = audio.open(format=FORMAT, channels=CHANNELS,
                         rate=RATE, input=True,
-                        frames_per_buffer=CHUNK)
+                        frames_per_buffer=CHUNK,
+                        input_device_index=0)
     st.session_state.stream = stream
     st.session_state.frames = []
     st.success("Recording started")
